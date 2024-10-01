6a293bbfa7e5dedb40765881c6c8dfc6b466a994
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-10-01 09:14:47 +0800

    feat: install @paolojulian.dev/design-system@1.2.3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,19 @@
 import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 const config: Pick<Config, 'content' | 'presets' | 'theme'> = {
-  presets: [require('@paolojulian.dev/design-system/tailwind-config/tailwind.config.js')],
+  presets: [
+    require('@paolojulian.dev/design-system/tailwind-config/tailwind.config.js'),
+  ],
   content: ['./src/**/*.tsx'],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        green: colors.green[400],
+        red: colors.red[400],
+      },
+    },
+  },
 };
 
 export default config;
