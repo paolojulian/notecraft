7fb7bdde99f9149e0159bf21dda03b6fc32ca3d2
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-10-01 09:20:21 +0800

    fix: eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  // parser: "@typescript-eslint/parser",
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
